b22d2893be7b48ee1b4a5bd95d1807ba2af49c27
Devendra Singh <devendrachaudhary684@gmail.com>
2022-12-14 21:47:31 +0530

    day-2

--- Mocker/server.js
+++ Mocker/server.js
@@ -9,7 +9,7 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/api", router);
+server.use("", router);
 server.listen(PORT, () =>
   console.log(`JSON Server is running on port ${PORT}`)
 );
