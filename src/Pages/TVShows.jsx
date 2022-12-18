6bb0b357e87b50b01c79fc56bbd63eaf08e05011
Devendra Singh <devendrachaudhary684@gmail.com>
2022-12-19 01:53:09 +0530

    complete

--- src/Pages/TVShows.jsx
+++ src/Pages/TVShows.jsx
@@ -4,124 +4,124 @@ import Navbar from "../Components/Navbar";
 import Footer from "./Footer";
 const music = [
   {
-    video: "https://www.youtube.com/embed/Sb9SsxBPBEU",
-    title: "Diljit Dosanjh Lemonade (Visualiser) | Drive Thru",
+    video: "https://youtu.be/P8qF8C8PeZ8",
+    title: "Mett | Zee TV Premiere",
   },
   {
-    video: "https://www.youtube.com/embed/RLhuPD2ASKE",
-    title: "We Rollin (Official Audio) - Shubh",
+    video: "https://youtu.be/QVaLERuq19Y",
+    title: "Kundali Bhagya | Zee TV Premiere",
   },
   {
-    video: "https://www.youtube.com/embed/OP4AQqC8ss4",
-    title: "Shubh - Baller (Official Music Video) ",
+    video: "https://youtu.be/pnfjSj99dpU",
+    title: "राधाकृष्ण | RadhaKrishn | Zee TV Premiere",
   },
   {
-    video: "https://www.youtube.com/embed/wujewK7E0dc",
-    title: "Judge : Mankirt Aulakh (Official Video)",
+    video: "https://youtu.be/GRspb2J-55M",
+    title: "Rabb Se Hai Dua | Zee TV Premiere",
   },
   {
-    video: "https://www.youtube.com/embed/2w5kg8BfSD0",
-    title: "Mankirt Aulakh: Jail Official Song",
+    video: "https://www.youtube.com/embed/pnfjSj99dpU",
+    title: "Mett | Zee TV Premiere",
   },
   {
-    video: "https://www.youtube.com/embed/M8vDwlHigJA",
-    title: "GOAT (Full Video) Sidhu Moose Wala",
+    video: "https://www.youtube.com/embed/GRspb2J-55M",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/H6tFxphsZQs",
-    title: "MANKIRT AULAKH - JATT DI CLIP",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/vg0ZfeszGrU",
-    title: "Difference | Amrit Maan ft Sonia Maan",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/tpFljbJxZiw",
-    title: "LEVELS - Official Video",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/D4fC4HVpP3M",
-    title: "Its All About You | Sidhu Moose Wala",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/T-ztCxK4H00",
-    title: "King - Desi Dan Bilzerian",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/8Z8qobg8UdA",
-    title: "Get Up Jawani- Yo Yo Honey Singh",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/PJGVDaSaQWQ",
-    title: "'2 Many Girls' FULL VIDEO SONG",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/uaCYeQ9FtSI",
-    title: "Official: Issey Kehte Hain Hip Hop",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/ReXw6TOnUOc",
-    title: "All Black Full Song",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/i2GC06euEDE",
-    title: "Jaguar | Muzical Doctorz Sukhe Feat Bohemia",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/iMdH_G4N9nY",
-    title: "Wakhra Swag | Official Video",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/OulN7vTDq1I",
-    title: "Badshah - DJ Waley Babu",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/bzW9fmwcmG4",
-    title: "Daru Badnaam | Kamal Kahlon & Param Singh",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/q1wK0r51ARs",
-    title: "Gabru - J Star ft Yo Yo Honey Singh",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/NrXdauEv9HY",
-    title: "Dope Shope - Yo Yo Honey Singh and Deep Money",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/Wr2NN4VQ1nI",
-    title: "Daaru Party (Full Song) | Millind Gaba",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/TwFBtV13KQQ",
-    title: "One Bottle Down' Full Song with LYRICS",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/EpJk_dSpjVM",
-    title: "Yo Yo Honey Singh - Break Up Party",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/-j0dlcfekqw",
-    title: "Otilia - Bilionera (official video)",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/JOX09U8noOE",
-    title: "Imran Khan - Pata Chalgea (Un-Official Video)",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/JYodEWUdIso",
-    title: "Imran khan - Bewafa (Official Music Video)",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/A66TYFdz8YA",
-    title: "King - Tu Aake Dekhle | The Carnival",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/PWaPCqeCfeY",
-    title: "Sulthan Video Song (Hindi) | KGF Chapter 2",
+    title: "Mett | Zee TV Premiere",
   },
   {
     video: "https://www.youtube.com/embed/BJahIa255qc",
-    title: "Pushpa: Eyy Bidda Ye Mera Adda (Video) |",
+    title: "Mett | Zee TV Premiere",
   },
 ];
 
